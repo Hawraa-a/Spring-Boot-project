4ff17a22b11106bcf039c40c59bb4ef9febb66b6
Hawraa <hawra8516@gmail.com>
2025-11-17 15:12:10 +0400

    In-memory CRUD Apis

---
+++ ccsb/src/main/java/com/codeline/ccsb/HelloController.java
@@ -0,0 +1,45 @@
+package com.codeline.ccsb;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+public class HelloController {
+    private Map<Integer, String> courses = new HashMap<>();
+    private int idCounter = 1;
+
+    @PostMapping("create")
+    public String createStudent(@RequestParam String name) {
+        courses.put(idCounter, name);
+        return "Course created with ID: " + idCounter++;
+    }
+
+    @GetMapping("getAll")
+    public Map<Integer, String> getAllCourses() {
+        return courses;
+    }
+
+    @GetMapping("getById")
+    public String getCourses(@RequestParam int id) {
+        return courses.getOrDefault(id, "Course not found");
+    }
+
+    @PutMapping("update")
+    public String updateCourse(@RequestParam int id, @RequestParam String name) {
+        if (courses.containsKey(id)) {
+            courses.put(id, name);
+        }
+        return "Course updated successfully";
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteCourse(@PathVariable int id) {
+        if (courses.remove(id) != null) {
+            return "Course deleted successfully";
+        }
+        return "Course not found";
+    }
+}

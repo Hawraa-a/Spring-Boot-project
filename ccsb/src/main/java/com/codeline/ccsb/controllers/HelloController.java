e2c7c60b33827f51f76e8a2e5f768c33f7a44348
Hawraa <hawra8516@gmail.com>
2025-11-23 15:58:04 +0400

    Change code to use database for data

---
+++ ccsb/src/main/java/com/codeline/ccsb/controllers/HelloController.java
@@ -0,0 +1,44 @@
+package com.codeline.ccsb.controllers;
+
+import com.codeline.ccsb.entities.Course;
+import com.codeline.ccsb.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HelloController {
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping("create")
+    public Course createCourses(@RequestBody Course requestObj) {
+        Course course = courseService.saveCourse(requestObj);
+        return course;
+    }
+
+    @GetMapping("getAll")
+    public List<Course> getAllCourses() {
+        List<Course> courseList = courseService.getAllCourses();
+        return courseList;
+    }
+
+    @GetMapping("getById")
+    public Course getCourses(@RequestParam int id) throws Exception{
+        return courseService.getCourseById(id);
+    }
+
+    @PutMapping("update")
+    public Course updateCourse(@RequestBody Course updateObjFromUser) throws Exception{
+        return courseService.updateCourse(updateObjFromUser);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteCourse(@PathVariable int id) throws Exception{
+        courseService.deleteCourse(id);
+        return "SUCCESS";
+    }
+}
---
+++ ccsb/src/main/java/com/codeline/ccsb/entities/Course.java
@@ -0,0 +1,29 @@
+package com.codeline.ccsb.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String name;
+    String category;
+    Integer duration;
+    Date createdDate;
+    Date updatedDate;
+    Boolean isActive;
+}
